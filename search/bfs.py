c2de14abe1465d04cee4911e3779f5eb99c26719
Wonseok Cho <59154288+jws020501@users.noreply.github.com>
2026-06-15 01:32:08 +0900

    Add files via upload

---
+++ search/bfs.py
@@ -0,0 +1,34 @@
+class bfs():
+    def __init__(self, graph, start_node=1):
+        self.graph = graph
+        self.visited = [False] * len(graph)
+        self.start_node = start_node
+
+    def bfs(self, node):
+        queue = [node]
+        self.visited[node] = True
+        while queue:
+            current = queue.pop(0)
+            print(current, end=' ')
+            for neighbor in self.graph[current]:
+                if not self.visited[neighbor]:
+                    queue.append(neighbor)
+                    self.visited[neighbor] = True
+
+class Graph:
+    def __init__(self, bidirectional=False):
+        num_node, num_edge = map(int, input().split())
+        self.graph = [[] for _ in range(num_node + 1)]
+        for _ in range(num_edge):
+            u, v = map(int, input().split())
+            self.graph[u].append(v)
+            if bidirectional:
+                self.graph[v].append(u)
+
+if __name__ == "__main__":
+    is_directed = input(">> 방향성 여부 (y/n): ").lower() == 'y'
+    g = Graph(is_directed).graph
+    start_node = int(input(">> 시작노드:"))
+    print("BFS 탐색 결과:",end=' ')
+    bfs(g, start_node).bfs(start_node)
+    
